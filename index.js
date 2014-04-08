2a399574ecfeca3248447e96c656e59db39dd5b2
netpoetica <krosenberg@m3-health.com>
2014-04-08 09:08:56 -0400

    Removing require from comments in the index.js because this messes up component build

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-/**
-* JSONfn - javascript (both node.js and browser) plugin to stringify, 
-*          parse and clone objects with Functions, Regexp and Date.
-*  
-* Version - 0.60.00
-* Copyright (c) 2012 - 2014 Vadim Kiryukhin
-* vkiryukhin @ gmail.com
-* http://www.eslinstructor.net/jsonfn/
-* 
-* Licensed under the MIT license ( http://www.opensource.org/licenses/mit-license.php )
-*
-*   USAGE:
-*     browser:
-*         JSONfn.stringify(obj);
-*         JSONfn.parse(str[, date2obj]);
-*         JSONfn.clone(obj[, date2obj]);
-*
-*     nodejs:
-*       var JSONfn = require('path/to/json-fn');
-*       JSONfn.stringify(obj);
-*       JSONfn.parse(str[, date2obj]);
-*       JSONfn.clone(obj[, date2obj]);
-*
-*
-*     @obj      -  Object;
-*     @str      -  String, which is returned by JSONfn.stringify() function; 
-*     @date2obj - Boolean (optional); if true, date string in ISO8061 format
-*                 is converted into a Date object; otherwise, it is left as a String.
-*/
-
 "use strict";
 
 exports.stringify = function (obj) {
